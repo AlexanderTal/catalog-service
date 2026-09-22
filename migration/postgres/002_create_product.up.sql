36c41bf92df62e9cae234c423e11f91016dcba7a
Alexander <thebeatles.help@yandex.ru>
2026-09-22 21:41:23 +0300

    TASK-1004: Add UNIQUE to product.name, fix price check

--- migration/postgres/002_create_product.up.sql
+++ migration/postgres/002_create_product.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS product (
-id            BIGSERIAL    NOT NULL UNIQUE,
-guid          UUID         NOT NULL PRIMARY KEY,
-name          VARCHAR(255) NOT NULL,
-description   TEXT,
-price         BIGINT       NOT NULL CHECK (price >= 0),
-category_guid UUID         NOT NULL REFERENCES category(guid) ON DELETE RESTRICT,
-created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW()
-);
+                                       id            BIGSERIAL    NOT NULL UNIQUE,
+                                       guid          UUID         NOT NULL PRIMARY KEY,
+                                       name          VARCHAR(255) NOT NULL UNIQUE,
+    description   TEXT,
+    price         BIGINT       NOT NULL CHECK (price > 0),
+    category_guid UUID         NOT NULL REFERENCES category(guid) ON DELETE RESTRICT,
+    created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW()
+    );
 
 CREATE INDEX IF NOT EXISTS idx_product_category_guid ON product (category_guid);
